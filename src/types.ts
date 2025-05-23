90d2e747de3d67b322485676e6bd6fca25f35507
Jonathanmasa <jonathanmasa97@gmail.com>
2025-05-23 20:34:39 +0200

    add nationality and actress types to types.ts

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,26 @@ export type Person = {
   biography: string;
   image: string;
 };
+
+
+const validNationalities = [
+  "American",
+  "British",
+  "Australian",
+  "Israeli-American",
+  "South African",
+  "French",
+  "Indian",
+  "Israeli",
+  "Spanish",
+  "South Korean",
+  "Chinese"
+] as const;
+
+export type Nationality = typeof validNationalities[number];
+
+export type Actress = Person & {
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality: Nationality;
+};
